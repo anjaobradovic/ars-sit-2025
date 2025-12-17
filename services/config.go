459bb9b4651cfbf6d57df7fa6888553f38d4596d
Anja Obradovic <163130723+anjaobradovic@users.noreply.github.com>
2025-12-17 23:47:25 +0100

    fixed add config

--- services/config.go
+++ services/config.go
@@ -1,11 +1,30 @@
 package services
 
 import (
+	"errors"
+
 	"github.com/anjaobradovic/ars-sit-2025/model"
 	"github.com/anjaobradovic/ars-sit-2025/repositories"
 )
 
-func CreateConfig(c model.Config) error {
-	// koristi AddConfig iz repositories
-	return repositories.Save(c)
+type ConfigService struct {
+	repo *repositories.ConfigRepository
+}
+
+func NewConfigService(repo *repositories.ConfigRepository) *ConfigService {
+	return &ConfigService{repo: repo}
+}
+
+func (s *ConfigService) Create(config model.Config) error {
+	if config.ID == "" {
+		return errors.New("id is required")
+	}
+	if config.Name == "" {
+		return errors.New("name is required")
+	}
+	if config.Version == "" {
+		return errors.New("version is required")
+	}
+
+	return s.repo.Save(config)
 }
